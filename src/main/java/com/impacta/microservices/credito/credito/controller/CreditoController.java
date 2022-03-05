c8752c5e6fe7d62f9efb1bd29755c0c7f77948af
Carla Regina <carlareginam10@gmail.com>
2022-03-05 19:04:14 -0300

    update controller/service/repository

---
+++ src/main/java/com/impacta/microservices/credito/credito/controller/CreditoController.java
@@ -0,0 +1,71 @@
+package com.impacta.microservices.credito.credito.controller;
+
+
+import java.util.List;
+import com.impacta.microservices.credito.credito.controller.response.SaldoCreditoResponse;
+import com.impacta.microservices.credito.credito.domain.Credito;
+import com.impacta.microservices.credito.credito.service.CreditoService;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.web.bind.annotation.*;
+
+@Tag(name="Credito endpoint")
+@RestController
+@RequestMapping("/credito")
+public class CreditoController {
+
+    private final CreditoService creditoService;
+
+    public CreditoController(CreditoService creditoService) {
+        this.creditoService = creditoService;
+    }
+
+    @GetMapping(path = "/status_aplicacao")
+    public String checarStatusAplicacao(){
+        return "Aplicação online";
+    }
+
+    @PostMapping
+    public Credito criarCredito(@RequestBody Credito credito){
+        return creditoService.criarCredito(credito);
+    }
+
+    //GET PARA CONSULTAR TODAS AS TRANSACOES POR TIPO CONTA
+    @GetMapping(path = "/contas")
+    public List<Credito> consultaTransacoesPorTipo(@RequestParam(value = "tipoConta", required = true) String tipoConta) {
+        return creditoService.consultaTransacoesTipoConta(tipoConta);
+    }
+
+    //GET PARA CONSULTAR TODAS AS TRANSACOES DAS CONTAS
+    @GetMapping(path = "/listar/contas")
+    public List<Credito> listarContas() {
+        return creditoService.listarContas();
+    }
+
+    //GET PARA CONSULTAR TRANSACOES DE CREDITO POR CONTA CORRENTE
+    @GetMapping(path = "/extrato/contacorrente/{contaId}")
+    public List<Credito> consultaExtratoContaCorrente(@PathVariable("contaId") Integer contaId) {
+        return  creditoService.consultaContaIdContaCorrente(contaId);
+    }
+
+    //GET PARA CONSULTAR TRANSACOES DE CREDITO POR CONTA INVESTIMENTO
+    @GetMapping(path = "/extrato/investimento/{contaId}")
+    public List<Credito> consultaExtratoContaInvestimento(@PathVariable("contaId") Integer contaId) {
+        return creditoService.consultaContaIdInvestimento(contaId);
+    }
+
+    //GET PARA CONSOLIDAR OS VALORES DE CREDITO EM UMA CONTA CORRENTE
+    @GetMapping(path = "/saldo/contacorrente/{contaId}")
+    public SaldoCreditoResponse consultaSaldoConsolidadoContaCorrente(@PathVariable("contaId") Integer contaId) {
+        SaldoCreditoResponse saldoCreditoResponse =
+                new SaldoCreditoResponse(creditoService.consultaSaldoContaIdContaCorrente(contaId));
+        return  saldoCreditoResponse;
+    }
+
+    //GET PARA CONSOLIDAR OS VALORES DE CREDITO EM UMA CONTA INVESTIMENTO
+    @GetMapping(path = "/saldo/investimento/{contaId}")
+    public SaldoCreditoResponse consultaSaldoConsolidadoContaInvestimento(@PathVariable("contaId") Integer contaId) {
+        SaldoCreditoResponse saldoCreditoResponse =
+                new SaldoCreditoResponse(creditoService.consultaSaldoContaIdContaInvestimento(contaId));
+        return saldoCreditoResponse;
+    }
+}
---
+++ src/main/java/com/impacta/microservices/credito/credito/service/CreditoService.java
@@ -0,0 +1,75 @@
+package com.impacta.microservices.credito.credito.service;
+
+import java.util.List;
+
+import com.impacta.microservices.credito.credito.domain.Credito;
+import com.impacta.microservices.credito.credito.domain.TipoConta;
+
+import com.impacta.microservices.credito.credito.repository.CreditoRepository;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class CreditoService {
+
+    private CreditoRepository repository;
+    public CreditoService(CreditoRepository repository) {
+        this.repository = repository;
+    }
+
+    public Credito criarCredito(Credito credito){
+
+        if(!credito.getTipoConta().equals(TipoConta.contacorrente.toString()) && !credito.getTipoConta().equals(TipoConta.investimento.toString())) {
+            throw new TipoContaBadRequestException("Tipo de conta iválido, por favor digitar contacorrente ou investimento");
+        }
+        return repository.save(credito);
+    }
+
+    public List<Credito> listarContas() {return repository.listarContas();}
+
+    public List<Credito> consultaTransacoesTipoConta(String tipoConta) {
+
+        if(!tipoConta.equals(TipoConta.contacorrente.toString()) && !tipoConta.equals(TipoConta.investimento.toString())) {
+            throw new TipoContaNotFoundException("Tipo de conta incorreta, por favor pesquisar por tipo contacorrente ou investimento");
+        }
+
+        var contas = repository.findByTipoConta(tipoConta.toString());
+        return contas;
+    }
+
+    public List<Credito> consultaContaIdContaCorrente(Integer contaId) {
+
+        var listCredito = repository.findByContaIdAndTipoConta(contaId, "contacorrente");
+
+        if (listCredito.isEmpty()) {
+            throw new ContaIdNotFoundException("Não encontrada conta id: " + contaId);
+        }
+        return listCredito;
+    }
+
+    public List<Credito> consultaContaIdInvestimento(Integer contaId) {
+
+        var listCredito = repository.findByContaIdAndTipoConta(contaId, "investimento");
+
+        if(listCredito.isEmpty()) {
+            throw new ContaIdNotFoundException("Não encontrada conta id: " + contaId);
+        }
+        return listCredito;
+    }
+
+    public double consultaSaldoContaIdContaCorrente(Integer contaId) {
+        try{
+            return  repository.findBySaldoCreditoPorTipoConta(contaId, "contacorrente");
+        } catch (RuntimeException runtimeException) {
+            throw new ContaIdNotFoundException("Não encontrada conta id: " + contaId);
+        }
+    }
+
+    public double consultaSaldoContaIdContaInvestimento(Integer contaId) {
+        try{
+            return  repository.findBySaldoCreditoPorTipoConta(contaId, "investimento");
+        }catch (RuntimeException runtimeException) {
+            throw new ContaIdNotFoundException("Não encontrada conta id: " + contaId);
+        }
+    }
+}
